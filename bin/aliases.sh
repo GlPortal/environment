2b90c281ffa030c6c2332c2b07ecb36e86f18b20
Henry Hirsch <henry@w3-net.de>
2018-04-16 18:04:18 +0200

    Add very basic help command

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 DIR="'$(cd "$(dirname "$0")/../" && pwd | sed "s/'/'\\\''/g")'"
+alias rx-help="$DIR/bin/help.sh"
 alias rx-update="$DIR/bin/clone-or-pull-repositories.sh"
 alias rx-clone="$DIR/bin/clone.sh"
 alias rx-delete="$DIR/bin/delete.sh"
